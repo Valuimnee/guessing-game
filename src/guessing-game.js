f80892cf2cbea9b6a2c074054f8dbfa9485dce0d
Valuimnee <tsalapovadisco@gmail.com>
2017-09-23 07:16:16 +0300

    Change gitignore

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,12 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+		this.guesses=[];
+		this.last;
+	}
 
     setRange(min, max) {
-
+		this.min=min;
+		this.max=max;
+		this.last=this.min-1;
     }
 
     guess() {
-
+		if(this.guesses.length==0){
+			this.last=(this.min+this.max)/2;
+			this.guesses.push(this.last);
+			return this.last;
+		}
+			
     }
 
     lower() {
